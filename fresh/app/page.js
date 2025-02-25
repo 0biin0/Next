ebd692b09490d2129a18bd3568634489fbe9345d
0biin0 <infinite402@gmail.com>
2025-02-25 19:17:47 +0900

    commit

---
+++ fresh/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name = 'park'
+  return (
+    <div>
+      <h4 className="title">Bin's Fresh</h4>
+      <p className="title-sub">by dev kim {name}</p>
+    </div>
+  );
+}
